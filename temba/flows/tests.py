1d04cb5f6bb72072a33c6729475fc2f87ede5013
Norbert Kwizera <norkans7@gmail.com>
2015-11-10 22:43:19 +0200

    more tests

--- temba/flows/tests.py
+++ temba/flows/tests.py
@@ -3225,6 +3225,15 @@ class FlowsTest(FlowFileTest):
         self.assertEquals(1, len(runs))
         self.assertEquals(0, self.contact.msgs.all().count())
 
+        Msg.objects.all().delete()
+
+        flow = self.get_flow('contact-update-by-mock')
+        runs = flow.start_msg_flow(Contact.objects.filter(id=self.contact.id))
+        self.assertEquals(1, len(runs))
+        self.assertEquals(0, self.contact.msgs.all().count())
+        contact = Contact.objects.get(id=self.contact.pk)
+        self.assertEquals("+250 785 551 212", contact.get_field('via').string_value)
+
     def test_new_contact(self):
         mother_flow = self.get_flow('mama_mother_registration')
         registration_flow = self.get_flow('mama_registration', dict(NEW_MOTHER_FLOW_ID=mother_flow.pk))
