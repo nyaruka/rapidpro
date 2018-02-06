42fcf0b222802865d7ff31305e48ed3c328fe86e
Norbert Kwizera <norkans7@gmail.com>
2018-02-06 17:32:50 +0200

    Move FCM config to type configuration blurb

--- temba/channels/types/firebase/type.py
+++ temba/channels/types/firebase/type.py
@@ -25,6 +25,33 @@ class FirebaseCloudMessagingType(ChannelType):
 
     claim_blurb = _("""Add a <a href="https://firebase.google.com/docs/cloud-messaging/" target="_blank"> Firebase Cloud
     Messaging Channel</a> to send and receive messages. Your users will need an App to send and receive messages.""")
+
+    configuration_blurb = _(
+        """
+        <h4>
+        To use your Firebase Cloud Messaging channel you'll have to POST to the following URLs with the parameters below.
+        </h4>
+
+        <div class="clearfix"></div>
+        <hr/>
+
+        <h4>Contact Register</h4>
+
+        <p>To register contacts, POST to the following URL with the parameters urn, fcm_token and optionally name.</p>
+
+        <code>https://{{ channel.callback_domain }}{% url 'handlers.fcm_handler' 'register' channel.uuid %}</code>
+
+        <h4>Receive URL</h4>
+
+        <p>To handle incoming messages, POST to the following URL with the parameters from, msg and fcm_token.</p>
+
+        <code>https://{{ channel.callback_domain }}{% url 'handlers.fcm_handler' 'receive' channel.uuid %}</code>
+
+        <hr/>
+
+        """
+    )
+
     claim_view = ClaimView
 
     schemes = [FCM_SCHEME]
