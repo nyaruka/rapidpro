5c545350d26f3bd6e1acc00dbbfa8e9ec28a7272
Norbert Kwizera <norkans7@gmail.com>
2018-02-06 17:50:48 +0200

    Move ZV config to type configuration blurb

--- temba/channels/types/zenvia/type.py
+++ temba/channels/types/zenvia/type.py
@@ -20,6 +20,61 @@ class ZenviaType(ChannelType):
     name = "Zenvia"
 
     claim_blurb = _("""If you are based in Brazil, you can purchase a short code from <a href="http://www.zenvia.com.br/">Zenvia</a> and connect it in a few simple steps.""")
+
+    configuration_blurb = _(
+        """
+        <h4>
+        To finish configuring your Zenvia connection you'll need to set the following callback URLs on your Zenvia account."
+        </h4>
+
+        <div class="clearfix">
+            <div class="config-value">
+                <div class="name">
+                Account:
+                </div>
+                <div class="value">
+                {{ channel.config_json.account }}
+                </div>
+            </div>
+            <div class="config-value">
+                <div class="name">
+                Code:
+                </div>
+                <div class="value">
+                {{ channel.config_json.code }}
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="clearfix"></div>
+        <hr/>
+
+        <h4>Status URL</h4>
+
+        <p>
+        To receive delivery and acknowledgement of sent messages, you need to set the status URL
+        for your Zenvia account.
+        </p>
+
+        <code>https://{{ channel.callback_domain }}{% url 'courier.zv' channel.uuid 'status' %}</code>
+
+        <hr/>
+
+        <h4>Receive URL</h4>
+
+        <p>
+        To receive incoming messages, you need to set the receive URL for your Zenvia account.
+        </p>
+
+        <code>https://{{ channel.callback_domain }}{% url 'courier.zv' channel.uuid 'receive' %}</code>
+
+        <hr/>
+
+        """
+    )
+
     claim_view = ClaimView
 
     schemes = [TEL_SCHEME]
