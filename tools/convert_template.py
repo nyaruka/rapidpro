cac8c54b21ffbbc927f9802a401c92de9060a17c
Eric Newcomer <ericnewcomer@gmail.com>
2023-05-12 11:21:06 -0700

    Delete correct file

--- tools/convert_template.py
+++ tools/convert_template.py
@@ -13,7 +13,37 @@ sad_template = re.compile("\\{\n\\s+%")
 sad_files = {}
 
 
-def convert_template(haml_path: str, *, format: bool, delete: bool):
+def check_for_sad(path: str):
+    # check for sad templates
+    formatted = open(path, "r")
+    formatted_text = formatted.read()
+    formatted.close()
+    matches = sad_template.findall(formatted_text)
+    if matches:
+        sad_files[path] = len(matches)
+
+
+def format_path(path: str, *, delete: bool):
+    os.system(f"djlint --profile=django --reformat --quiet {path}")
+    if os.path.isdir(path):
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in files:
+                if name.endswith(".html"):
+                    filename = os.path.join(root, name)
+                    check_for_sad(filename)
+
+                    if delete and filename not in sad_files:
+                        haml_file = filename.replace(".html", ".haml")
+                        if os.path.exists(haml_file):
+                            os.remove(filename.replace(".html", ".haml"))
+    else:
+        if name.endswith(".html"):
+            check_for_sad(path)
+            if delete and path not in sad_files:
+                os.remove(path)
+
+
+def convert_template(haml_path: str):
     html_path = os.path.splitext(haml_path)[0] + ".html"
 
     with open(haml_path, "r") as file:
@@ -25,34 +55,11 @@ def convert_template(haml_path: str, *, format: bool, delete: bool):
         file.write(html_content)
 
 
-def check_for_sad(path: str):
-    # check for sad templates
-    formatted = open(path, "r")
-    formatted_text = formatted.read()
-    formatted.close()
-    matches = sad_template.findall(formatted_text)
-    if matches:
-        sad_files[path] = len(matches)
-
-
-def format_path(path: str):
-    os.system(f"djlint --profile=django --reformat --quiet {path}")
-
-    if os.path.isdir(path):
-        for root, dirs, files in os.walk(path, topdown=False):
-            for name in files:
-                if name.endswith(".html"):
-                    check_for_sad(os.path.join(root, name))
-    else:
-        check_for_sad(path)
-
-
-def convert_directory(path: str, *, format: bool, delete: bool):
+def convert_directory(path: str):
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             if name.endswith(".haml"):
-                convert_template(os.path.join(root, name), format=format, delete=delete)
-    format_path(path)
+                convert_template(os.path.join(root, name))
 
 
 def print_sad():
@@ -61,10 +68,10 @@ def print_sad():
 
 
 if os.path.isdir(source):
-    convert_directory(source, format=True, delete=True)
+    convert_directory(source)
+    format_path(source, delete=True)
     print_sad()
-
 else:
-    convert_template(source, format=True, delete=True)
-    format_path(source)
+    convert_template(source)
+    format_path(source, delete=True)
     print_sad()
