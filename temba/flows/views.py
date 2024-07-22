a2c9af91e64ae69a81597cf07b5223a39299f7b8
Eric Newcomer <ericnewcomer@gmail.com>
2024-07-22 12:54:01 -0700

    Add flow view for next editor

--- temba/flows/views.py
+++ temba/flows/views.py
@@ -188,6 +188,7 @@ class FlowCRUDL(SmartCRUDL):
         "import_translation",
         "export_results",
         "editor",
+        "next",
         "results",
         "category_counts",
         "preview_start",
@@ -966,6 +967,9 @@ class FlowCRUDL(SmartCRUDL):
 
                 if self.has_org_perm("flows.flow_update"):
                     menu.add_link(_("Import Translation"), reverse("flows.flow_import_translation", args=[obj.id]))
+
+    class Next(Editor, SpaMixin):
+        template_name = "flows/flow_next.html"
 
     class ChangeLanguage(OrgObjPermsMixin, SmartUpdateView):
         class Form(forms.Form):
