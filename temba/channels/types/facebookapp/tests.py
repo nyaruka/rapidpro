e37fbd3b35be5eb3d96972f3e1c577b8216c3044
Norbert Kwizera <norkans7@gmail.com>
2024-11-27 13:26:17 +0200

    Add check_credentials tests

--- temba/channels/types/facebookapp/tests.py
+++ temba/channels/types/facebookapp/tests.py
@@ -323,3 +323,45 @@ class FacebookTypeTest(TembaTest):
             "https://developers.facebook.com/docs/messenger-platform/error-codes",
             FacebookAppType().get_error_ref_url(None, "190"),
         )
+
+    @override_settings(FACEBOOK_APPLICATION_ID="FB_APP_ID", FACEBOOK_APPLICATION_SECRET="FB_APP_SECRET")
+    @patch("requests.get")
+    def test_check_credentials(self, mock_get):
+        check_credentials_url = reverse("channels.types.facebookapp.check_credentials", args=(self.channel.uuid,))
+
+        self.login(self.admin)
+
+        mock_get.return_value = MockResponse(200, json.dumps({"success": True, "data": {}}))
+        response = self.client.get(check_credentials_url)
+        self.assertContains(response, "Reconnect Facebook Page")
+        self.assertContains(
+            response, "Error with token, you need to reconnect the Facebook page by clicking the button below"
+        )
+        self.assertEqual(
+            response.context["update_token_url"], f"{reverse("channels.types.facebookapp.claim")}?update=1"
+        )
+        self.assertFalse(response.context["valid_token"])
+
+        mock_get.return_value = MockResponse(200, json.dumps({"success": True, "data": {"is_valid": True}}))
+
+        response = self.client.get(check_credentials_url)
+        self.assertContains(response, "Reconnect Facebook Page")
+        self.assertContains(response, "Everything looks good. No need to reconnect")
+        self.assertEqual(
+            response.context["update_token_url"], f"{reverse("channels.types.facebookapp.claim")}?update=1"
+        )
+        self.assertTrue(response.context["valid_token"])
+
+    @override_settings(FACEBOOK_APPLICATION_ID="FB_APP_ID", FACEBOOK_APPLICATION_SECRET="FB_APP_SECRET")
+    @patch("requests.get")
+    def test_type_check_credentials(self, mock_get):
+        self.assertFalse(FacebookAppType().check_credentials({}))
+
+        mock_get.return_value = MockResponse(200, json.dumps({"success": True, "data": {}}))
+        self.assertFalse(FacebookAppType().check_credentials({Channel.CONFIG_AUTH_TOKEN: "Token"}))
+
+        mock_get.return_value = MockResponse(400, json.dumps({"error": True}))
+        self.assertFalse(FacebookAppType().check_credentials({Channel.CONFIG_AUTH_TOKEN: "Token"}))
+
+        mock_get.return_value = MockResponse(200, json.dumps({"success": True, "data": {"is_valid": True}}))
+        self.assertTrue(FacebookAppType().check_credentials({Channel.CONFIG_AUTH_TOKEN: "Token"}))
