1cd7b39a496620383fbd09f082cc58b6300df8a6
Rowan Seymour <rowanseymour@gmail.com>
2025-02-26 19:36:00 +0000

    Remove task to interrupt sessions as this is now handled by session expiration fires

--- temba/mailroom/queue.py
+++ temba/mailroom/queue.py
@@ -84,20 +84,18 @@ def queue_interrupt_channel(org, channel):
     _queue_batch_task(org.id, BatchTask.INTERRUPT_CHANNEL, task, HIGH_PRIORITY)
 
 
-def queue_interrupt(org, *, contacts=None, flow=None, sessions=None):
+def queue_interrupt(org, *, contacts=None, flow=None):
     """
     Queues an interrupt task for handling by mailroom
     """
 
-    assert contacts or flow or sessions, "must specify either a set of contacts or a flow or sessions"
+    assert contacts or flow, "must specify either a set of contacts or a flow"
 
     task = {}
     if contacts:
         task["contact_ids"] = [c.id for c in contacts]
     if flow:
         task["flow_ids"] = [flow.id]
-    if sessions:
-        task["session_ids"] = [s.id for s in sessions]
 
     _queue_batch_task(org.id, BatchTask.INTERRUPT_SESSIONS, task, HIGH_PRIORITY)
 
