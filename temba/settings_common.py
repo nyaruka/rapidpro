6ba19c4cbb28cde83bc9f432f8e1d2e2979b4fbf
Eric Newcomer <ericnewcomer@gmail.com>
2025-06-17 16:11:49 +0000

    Remove unnecessary permission

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -346,7 +346,7 @@ PERMISSIONS = {
     "contacts.contactfield": ("update_priority",),
     "contacts.contactgroup": ("menu",),
     "contacts.contactimport": ("preview",),
-    "flows.flow": ("assets", "copy", "editor", "export", "menu", "next", "results", "result_chart", "start"),
+    "flows.flow": ("assets", "copy", "editor", "export", "menu", "next", "results", "start"),
     "flows.flowstart": ("interrupt", "status"),
     "flows.flowsession": ("json",),
     "globals.global": ("unused",),
