a4f1054da64a979478a125585532c6a3d2db55f0
Rowan Seymour <rowanseymour@gmail.com>
2025-09-17 21:05:35 +0000

    Change ticket event backfill migration to save full user references

--- temba/tickets/tests/test_migrations.py
+++ temba/tickets/tests/test_migrations.py
@@ -180,7 +180,7 @@ class BackfillTicketEventsTest(MigrationTest):
                         "created_on": "2025-08-11T20:36:00+00:00",
                         "ticket_uuid": "01992f54-5ab6-717a-a39e-e8ca91fb7262",
                         "assignee": {"uuid": str(self.agent.uuid), "name": "Agnes"},
-                        "_user_uuid": str(self.admin.uuid),
+                        "_user": {"uuid": str(self.admin.uuid), "name": "Andy"},
                     },
                 },
                 {
@@ -223,7 +223,7 @@ class BackfillTicketEventsTest(MigrationTest):
                         "type": "ticket_reopened",
                         "created_on": "2025-08-11T20:36:00+00:00",
                         "ticket_uuid": "01992f54-5ab6-717a-a39e-e8ca91fb7262",
-                        "_user_uuid": str(self.admin.uuid),
+                        "_user": {"uuid": str(self.admin.uuid), "name": "Andy"},
                     },
                 },
                 {
@@ -235,7 +235,7 @@ class BackfillTicketEventsTest(MigrationTest):
                         "created_on": "2025-08-11T20:36:00+00:00",
                         "ticket_uuid": "01992f54-5ab6-717a-a39e-e8ca91fb7262",
                         "note": "We need to follow up",
-                        "_user_uuid": str(self.editor.uuid),
+                        "_user": {"uuid": str(self.editor.uuid), "name": "Ed McEdits"},
                     },
                 },
             ],
