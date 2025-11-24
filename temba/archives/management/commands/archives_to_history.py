1fa15f198594be05d8daa61d00fb0ed97ca5ad01
Rowan Seymour <rowanseymour@gmail.com>
2025-11-24 19:59:26 +0000

    Add archives_to_history management command to rewrite archives and import msg events from them

---
+++ temba/archives/management/commands/archives_to_history.py
@@ -0,0 +1,148 @@
+from datetime import date, datetime, timezone as tzone
+
+import iso8601
+
+from django.core.management.base import BaseCommand, CommandError
+
+from temba.archives.models import Archive
+from temba.orgs.models import Org
+from temba.utils import dynamo
+from temba.utils.uuid import uuid7
+
+tag_statuses = {"wired", "sent", "delivered", "read", "errored", "failed"}
+
+
+class Command(BaseCommand):
+    help = "Imports chat history events from message archives into DynamoDB"
+
+    def add_arguments(self, parser):
+        parser.add_argument("step", type=str, action="store", choices=("update", "import"))
+        parser.add_argument("--org", type=int, dest="org_id", default=None)
+        parser.add_argument("--since", type=date.fromisoformat)
+        parser.add_argument("--until", type=date.fromisoformat)
+
+    def handle(self, step: str, org_id: int, since: date, until: date, *args, **kwargs):
+        orgs = Org.objects.filter(is_active=True, is_suspended=False).exclude(archives=None).order_by("id")
+        if org_id:
+            orgs = orgs.filter(id=org_id)
+
+        since = datetime.combine(since, datetime.min.time(), tzinfo=tzone.utc)
+        until = datetime.combine(until, datetime.max.time())
+
+        self.stdout.write(f"Starting message archive {step} for {len(orgs)} orgs...")
+
+        for org in orgs:
+            if step == "update":
+                self.stdout.write(f" > updating archives for '{org.name}' (#{org.id})... ")
+                self.update_for_org(org, since, until)
+            else:
+                self.stdout.write(f" > importing archives for '{org.name}' (#{org.id})... ")
+                self.import_for_org(org, since, until)
+
+    def update_for_org(self, org, since=None, until=None):
+        archives = Archive._get_covering_period(org, Archive.TYPE_MSG, after=since, before=until)
+        for archive in archives:
+            self.stdout.write(
+                f"    - rewriting {archive.period}@{archive.start_date.isoformat()} #{archive.id}...", ending=""
+            )
+            self.stdout.flush()
+
+            progress = {"records": 0, "updated": 0}
+
+            def rewrite_msg(record) -> dict:
+                if "uuid" not in record:
+                    record["uuid"] = str(uuid7(when=iso8601.parse_date(record["created_on"])))
+                    progress["updated"] += 1
+
+                progress["records"] += 1
+
+                if progress["records"] % 10_000 == 0:
+                    self.stdout.write(".", ending="")
+                    self.stdout.flush()
+
+                return record
+
+            archive.rewrite(rewrite_msg, delete_old=True)
+
+            self.stdout.write(f" OK ({progress['records']} records, {progress['updated']} updated)")
+
+    def import_for_org(self, org, since=None, until=None):
+        with dynamo.HISTORY.batch_writer() as writer:
+            for record in Archive.iter_all_records(org, Archive.TYPE_MSG, after=since, before=until):
+                if "uuid" not in record:
+                    raise CommandError(f"Record for org #{org.id} has no UUID, cannot import")
+
+                contact_uuid = record["contact"]["uuid"]
+                event_uuid = record["uuid"]
+                event_time = record["created_on"]
+
+                if record["direction"] == "in":
+                    writer.put_item(
+                        self._item(
+                            org,
+                            contact_uuid,
+                            event_uuid,
+                            {"type": "msg_received", "created_on": event_time, "msg": self._msg(record)},
+                        )
+                    )
+
+                    if record["visibility"] == "deleted":
+                        writer.put_item(self._item(org, contact_uuid, event_uuid, {"created_on": event_time}, "del"))
+                else:
+                    if record["type"] in ("ivr", "voice"):
+                        writer.put_item(
+                            self._item(
+                                org,
+                                contact_uuid,
+                                event_uuid,
+                                {"type": "ivr_created", "created_on": event_time, "msg": self._msg(record)},
+                            )
+                        )
+                    else:
+                        msg = self._msg(record)
+                        writer.put_item(
+                            self._item(
+                                org,
+                                contact_uuid,
+                                event_uuid,
+                                {"type": "msg_created", "created_on": event_time, "msg": msg},
+                            )
+                        )
+
+                        if record["status"] in tag_statuses and "unsendable_reason" not in msg:
+                            writer.put_item(
+                                self._item(
+                                    org,
+                                    contact_uuid,
+                                    event_uuid,
+                                    {"created_on": event_time, "status": record["status"]},
+                                    "sts",
+                                )
+                            )
+
+    def _item(self, org, contact_uuid: str, event_uuid: str, data: dict, tag: str = None) -> dict:
+        # TODO use DataGZ for bigger payloads
+        return {
+            "PK": f"con#{contact_uuid}",
+            "SK": f"evt#{event_uuid}#{tag}" if tag else f"evt#{event_uuid}",
+            "OrgID": org.id,
+            "Data": data,
+        }
+
+    def _msg(self, record: dict) -> dict:
+        d = {"text": record.get("text", "")}
+
+        if urn := record.get("urn"):
+            d["urn"] = urn
+        if channel_ref := record.get("channel"):
+            d["channel"] = channel_ref
+        if attachments := record.get("attachments"):
+            d["attachments"] = attachments
+        if record.get("broadcast_id"):
+            # note that broadcasts are gone at this point, so we fabricate a UUID based on creation time
+            d["broadcast_uuid"] = uuid7(when=record["created_on"])
+
+        if record["direction"] == "out" and record["status"] == "failed" and "urn" not in d and "channel" not in d:
+            d["unsendable_reason"] = "no_route"
+
+        return d
--- temba/archives/management/commands/tests.py
+++ temba/archives/management/commands/tests.py
@@ -21,3 +21,152 @@ class SearchArchivesTest(TembaTest):
 
         self.assertIn('"id": 1', out.getvalue())
         self.assertIn("Fetched 2 records in", out.getvalue())
+
+
+class ArchivesToHistoryTest(TembaTest):
+    def test_command(self):
+        # run archive should be ignored
+        self.create_archive(
+            Archive.TYPE_FLOWRUN,
+            "D",
+            date(2025, 8, 1),
+            [{"id": 1, "created_on": "2020-07-30T10:00:00Z"}, {"id": 2, "created_on": "2020-07-30T15:00:00Z"}],
+        )
+
+        # message archive with old types
+        self.create_archive(
+            Archive.TYPE_MSG,
+            "D",
+            date(2015, 1, 1),
+            [
+                {
+                    # regular incoming message
+                    "id": 1,
+                    "broadcast": None,
+                    "contact": {"uuid": "dbe1460e-7e97-4db4-9944-3d8d20792a2d", "name": "Ann"},
+                    "urn": "twitter:kinyarwandanet",
+                    "channel": {"uuid": "d0c17405-a902-4a06-8fb8-5b067a582283", "name": "@rowanseymour"},
+                    "direction": "in",
+                    "type": "inbox",
+                    "status": "handled",
+                    "visibility": "visible",
+                    "text": "sawa",
+                    "attachments": [],
+                    "labels": [],
+                    "created_on": "2014-11-04T23:50:31+00:00",
+                    "sent_on": None,
+                    "modified_on": "2014-11-04T23:50:33.052089+00:00",
+                },
+                {
+                    # IVR incoming message
+                    "id": 2,
+                    "broadcast": None,
+                    "contact": {"uuid": "c33599af-2d97-4299-904d-2ea2d50921bb", "name": "Bob"},
+                    "urn": "tel:+1234567890",
+                    "channel": {"uuid": "d0c17405-a902-4a06-8fb8-5b067a582283", "name": "Twilio"},
+                    "direction": "in",
+                    "type": "ivr",
+                    "status": "handled",
+                    "visibility": "visible",
+                    "text": "who's there?",
+                    "attachments": [],
+                    "labels": [],
+                    "created_on": "2014-11-04T23:50:31+00:00",
+                    "sent_on": None,
+                    "modified_on": "2014-11-04T23:50:33.052089+00:00",
+                },
+                {
+                    # old surveyor style message with no channel and no urn
+                    "id": 3,
+                    "broadcast": None,
+                    "contact": {"uuid": "b9f65adb-efa4-4497-8527-7a7ff02df99c", "name": "Cat"},
+                    "urn": None,
+                    "channel": None,
+                    "direction": "in",
+                    "type": "flow",
+                    "status": "handled",
+                    "visibility": "visible",
+                    "text": "sawa 2",
+                    "attachments": [],
+                    "labels": [],
+                    "created_on": "2014-11-04T23:50:31+00:00",
+                    "sent_on": None,
+                    "modified_on": "2014-11-04T23:50:33.052089+00:00",
+                },
+            ],
+        )
+
+        # message archive with new types
+        self.create_archive(
+            Archive.TYPE_MSG,
+            "D",
+            date(2025, 1, 1),
+            [
+                {
+                    # unsendable message (no urn or channel)
+                    "id": 5297,
+                    "broadcast": 107746936,
+                    "contact": {"uuid": "d1f205ca-b299-4f41-8fb9-c19cbdb758e9", "name": "Rowan"},
+                    "urn": None,
+                    "channel": None,
+                    "flow": None,
+                    "direction": "out",
+                    "type": "text",
+                    "status": "failed",
+                    "visibility": "visible",
+                    "text": "Testing",
+                    "attachments": [],
+                    "labels": [],
+                    "created_on": "2025-04-02T17:00:02.931134+00:00",
+                    "sent_on": None,
+                    "modified_on": "2025-04-02T17:00:02.931523+00:00",
+                },
+                {
+                    "id": 5307,
+                    "broadcast": 23564,
+                    "contact": {"uuid": "a32d1f60-eb92-49c7-9161-4dd78e6a8b9e", "name": "Rowan"},
+                    "urn": "telegram:742307595",
+                    "channel": {"uuid": "ce4959aa-8c85-41a4-b53e-14c3f6852f90", "name": "RapidPro Test"},
+                    "flow": {"uuid": "448bb9d0-af76-4657-96b5-aa033805542d", "name": "Post Deploy Child"},
+                    "direction": "out",
+                    "type": "text",
+                    "status": "wired",
+                    "visibility": "visible",
+                    "text": "A cat uses its whiskers for measuring distances.",
+                    "attachments": [],
+                    "labels": [],
+                    "created_on": "2025-04-08T14:52:49.053541+00:00",
+                    "sent_on": "2025-04-08T14:52:53.773715+00:00",
+                    "modified_on": "2025-04-08T14:52:53.773715+00:00",
+                },
+            ],
+        )
+
+        # update 2015 archives
+        output = self._call(
+            "archives_to_history", "update", "--org", str(self.org.id), "--since", "2015-01-01", "--until", "2015-12-31"
+        )
+        self.assertIn("updating archives for 'Nyaruka'", output)
+        self.assertIn("rewriting D@2015-01-01", output)
+        self.assertIn("OK (3 records, 3 updated)", output)
+
+        # can run again and no updates needed
+        # output = self._call(
+        #    "archives_to_history", "update", "--org", str(self.org.id), "--since", "2015-01-01", "--until", "2015-12-31"
+        # )
+        # self.assertIn("updating archives for 'Nyaruka'", output)
+        # self.assertIn("rewriting D@2015-01-01", output)
+        # self.assertIn("OK (3 records, 0 updated)", output)
+
+        # update 2025 archives
+        output = self._call(
+            "archives_to_history", "update", "--org", str(self.org.id), "--since", "2025-01-01", "--until", "2025-12-31"
+        )
+        self.assertIn("updating archives for 'Nyaruka'", output)
+        self.assertIn("rewriting D@2025-01-01", output)
+        self.assertIn("OK (2 records, 2 updated)", output)
+
+    def _call(self, cmd, *args) -> str:
+        out = StringIO()
+        call_command(cmd, *args, stdout=out)
+        return out.getvalue()
