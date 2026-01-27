fafec56386f99827081b106f37205a2431e391ae
Norbert Kwizera <norkans7@gmail.com>
2026-01-27 16:01:00 +0200

    Add support for campaign delete

--- temba/campaigns/views.py
+++ temba/campaigns/views.py
@@ -20,6 +20,8 @@ from temba.orgs.views.base import (
     BaseReadView,
     BaseUpdateModal,
     BaseUpdateView,
+    OrgObjPermsMixin,
+    SmartDeleteView,
 )
 from temba.orgs.views.mixins import BulkActionMixin, OrgPermsMixin
 from temba.utils import languages
@@ -52,7 +54,7 @@ class CampaignForm(forms.ModelForm):
 
 class CampaignCRUDL(SmartCRUDL):
     model = Campaign
-    actions = ("create", "read", "update", "list", "archived", "archive", "activate", "menu")
+    actions = ("create", "read", "update", "list", "delete", "archived", "archive", "activate", "menu")
 
     class Menu(BaseMenuView):
         def derive_menu(self):
@@ -120,6 +122,14 @@ class CampaignCRUDL(SmartCRUDL):
 
                 if self.has_org_perm("orgs.org_export"):
                     menu.add_link(_("Export"), f"{reverse('orgs.org_export')}?campaign={obj.id}&archived=1")
+                if self.has_org_perm("campaigns.campaign_delete"):
+                    menu.new_group()
+                    menu.add_modax(
+                        _("Delete"),
+                        "campaign-delete",
+                        reverse("campaigns.campaign_delete", args=[obj.uuid]),
+                        title=_("Delete Campaign"),
+                    )
             else:
                 if self.has_org_perm("campaigns.campaignevent_create"):
                     menu.add_modax(
@@ -213,6 +223,22 @@ class CampaignCRUDL(SmartCRUDL):
         def save(self, obj):
             obj.apply_action_restore(self.request.user, Campaign.objects.filter(id=obj.id))
             return obj
+
+    class Delete(ModalFormMixin, OrgObjPermsMixin, SmartDeleteView):
+        cancel_url = "uuid@campaigns.campaign_read"
+        success_url = "@campaigns.campaign_list"
+        slug_url_kwarg = "uuid"
+        fields = ("uuid",)
+        submit_button_name = _("Delete")
+
+        def get_queryset(self, **kwargs):
+            qs = super().get_queryset(**kwargs)
+            return qs.filter(is_archived=True)
+
+        def post(self, request, *args, **kwargs):
+            self.object = self.get_object()
+            self.object.release(request.user)
+            return self.render_modal_response()
 
 
 class CampaignEventForm(forms.ModelForm):
